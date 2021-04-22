1843e9663cd9616c3036a0eb3f1463159f6a1c95
aurk0 <febtesham162125@bscse.uiu.ac.bd>
2021-04-22 20:37:17 +0600

    signUp

--- lib/registration/signIN.dart
+++ lib/registration/signIN.dart
@@ -7,8 +7,10 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
-  TextEditingController mail = new TextEditingController();
-  TextEditingController pass = new TextEditingController();
+  TextEditingController _emailcontroller1 = new TextEditingController();
+  TextEditingController _passcontroller1 = new TextEditingController();
+  TextEditingController _emailcontroller2 = new TextEditingController();
+  TextEditingController _passcontroller2 = new TextEditingController();
 
   final FirebaseAuth auth = FirebaseAuth.instance;
   final GlobalKey<FormState> _key = GlobalKey<FormState>();
@@ -20,83 +22,84 @@ class _SignInState extends State<SignIn> {
         title: Text("Chat App"),
         backgroundColor: Colors.teal[700],
       ),
-      body: Container(
-        child: Center(
-          child: Form(
-            key: _key,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextField(
-                    controller: mail,
-                    decoration: InputDecoration(
-                      hintText: "Enter E-mail",
-                    ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _key,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                TextField(
+                  controller: _emailcontroller1,
+                  decoration: InputDecoration(
+                    hintText: "Enter E-mail",
                   ),
-                  SizedBox(height: 10),
-                  TextField(
-                    controller: pass,
-                    decoration: InputDecoration(
-                      hintText: "Enter Password",
-                    ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  obscureText: true,
+                  controller: _passcontroller1,
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
                   ),
-                  SizedBox(
-                    height: 20,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    width: 150,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.blueGrey[700]),
+                    child: TextButton(
+                        onPressed: () async {
+                          if (_key.currentState.validate()) {
+                            regAccount();
+                          }
+                        },
+                        child: Text(
+                          "Sign Up",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ))),
+                Divider(
+                  height: 60,
+                  color: Colors.black,
+                ),
+                TextField(
+                  controller: _emailcontroller2,
+                  decoration: InputDecoration(
+                    hintText: "Enter E-mail",
                   ),
-                  Container(
-                      width: 150,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.blueGrey[700]),
-                      child: TextButton(
-                          onPressed: () async {
-                            if (_key.currentState.validate()) {
-                              regAccount();
-                            }
-                          },
-                          child: Text(
-                            "Sign Up",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
-                          ))),
-                  Divider(
-                    height: 60,
-                    color: Colors.black,
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  obscureText: true,
+                  controller: _passcontroller2,
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
                   ),
-                  TextField(
-                    controller: mail,
-                    decoration: InputDecoration(
-                      hintText: "Enter E-mail",
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  TextField(
-                    controller: pass,
-                    decoration: InputDecoration(
-                      hintText: "Enter Password",
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                      width: 150,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.blueGrey[700]),
-                      child: TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            "Sign In",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
-                          )))
-                ],
-              ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    width: 150,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.blueGrey[700]),
+                    child: TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Sign In",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        )))
+              ],
             ),
           ),
         ),
@@ -105,16 +108,14 @@ class _SignInState extends State<SignIn> {
   }
 
   void regAccount() async {
-    Future signUpWithEmailAndPassword(String email, String password) async {
-      try {
-        final User user = (await auth.createUserWithEmailAndPassword(
-                email: mail.text, password: pass.text))
-            .user;
-        print('Sign Up Successful');
-      } catch (e) {
-        print(e.toString());
-        return null;
-      }
+    try {
+      final User user = (await auth.createUserWithEmailAndPassword(
+              email: _emailcontroller1.text, password: _passcontroller1.text))
+          .user;
+      print('Sign Up Successful');
+    } catch (e) {
+      print(e.toString());
+      return null;
     }
   }
 }
